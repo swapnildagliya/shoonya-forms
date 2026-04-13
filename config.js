c06128a7b791c5bbbf6c1f4c3a87d7f0321fadb1
swapnildagliya <swapnildagliya@gmail.com>
2026-04-13 17:04:16 +0200

    up

--- config.js
+++
@@ -1,6 +0,0 @@
-// ============================================================
-// SHOONYA — Shared Configuration
-// Edit this file only. All forms read from here.
-// ============================================================
-
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzagQ6DrQzKsNzscEDlXxzMCOLTtJ1MloGm866pkK_Jv3aguggMRlOdr-c_QX0S5zxa/exec'; // paste your deployed Apps Script URL here
