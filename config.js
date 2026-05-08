a5830996d21758e88215b95643a71c33543561b5
swapnildagliya <swapnildagliya@gmail.com>
2026-05-08 18:15:09 +0200

    Update schedule

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // Edit this file only. All forms read from here.
 // ============================================================
 
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzagQ6DrQzKsNzscEDlXxzMCOLTtJ1MloGm866pkK_Jv3aguggMRlOdr-c_QX0S5zxa/exec'; // paste your deployed Apps Script URL here
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwh9PSrNxMUkBaMayhyfnU3XDzL76khEm7RL932CJ83qqm7dTG9afA-WB1cZYKSrcs3/exec'; // paste your deployed Apps Script URL here
